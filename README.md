8c87c849ab1d3c276f30731fd9dc6d54cb1ed5db
Chen Weiguang <chen.weiguang@gmail.com>
2020-02-09 21:38:36 +0800

    ci: switch to GitHub Actions
    Remove travis set-up and drop Python 2.7.
    
    Also add Spark 2.4.5 for vars.

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
 # Jupyter Spark Toree Notebook Docker Builder
 
+![CI Status](https://img.shields.io/github/workflow/status/guangie88/jupyter-pyspark-toree-docker/CI/master?label=CI&logo=github&style=for-the-badge)
+
 Dockerfile setup to install Jupyter with PySpark and Toree Kernel for Spark
 development.
 
 The base image used also contains some other useful Cloud SDK JARs, details can
 be found in the repo <https://github.com/guangie88/spark-custom-addons>.
 
-## How to Apply Travis Template
+## How to Apply Template for CI build
 
-For Linux user, you can download Tera CLI v0.2 at
+For Linux user, you can download Tera CLI v0.3 at
 <https://github.com/guangie88/tera-cli/releases> and place it in `PATH`.
 
 Otherwise, you will need `cargo`, which can be installed via
 [rustup](https://rustup.rs/).
 
-Once `cargo` is installed, simply run `cargo install tera-cli --version=^0.2.0`.
+Once `cargo` is installed, simply run `cargo install tera-cli --version=^0.3.0`.
--- push-images.sh
+++ push-images.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
+
 IMAGE_NAME=${IMAGE_NAME:-jupyter-pyspark-toree}
 
-HIVE_TAG_SUFFIX="$(if [ "${WITH_HIVE}" = "true" ]; then echo _hive; fi)"
-TAG_NAME="${JUPYTER_VERSION}_spark-${SPARK_VERSION}_scala-${SCALA_VERSION}_hadoop-${HADOOP_VERSION}_python-${PYTHON_VERSION}${HIVE_TAG_SUFFIX}_debian"
-
-docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
+TAG_NAME="${SELF_VERSION}_${JUPYTER_VERSION}_spark-${SPARK_VERSION}_scala-${SCALA_VERSION}_hadoop-${HADOOP_VERSION}_python-${PYTHON_VERSION}${HIVE_TAG_SUFFIX}_debian"
+docker tag "${IMAGE_NAME}:${TAG_NAME}" "${DOCKER_USERNAME}/${IMAGE_NAME}:${TAG_NAME}"
 docker push "${DOCKER_USERNAME}/${IMAGE_NAME}:${TAG_NAME}"
+
+ALT_TAG_NAME="${JUPYTER_VERSION}_spark-${SPARK_VERSION}_scala-${SCALA_VERSION}_hadoop-${HADOOP_VERSION}_python-${PYTHON_VERSION}${HIVE_TAG_SUFFIX}_debian"
+docker tag "${IMAGE_NAME}:${TAG_NAME}" "${DOCKER_USERNAME}/${IMAGE_NAME}:${ALT_TAG_NAME}"
+docker push "${DOCKER_USERNAME}/${IMAGE_NAME}:${ALT_TAG_NAME}"
