a860d054b6598eceb405bed2e7ff5dfaf1055a87
Chen Weiguang <chen.weiguang@gmail.com>
2020-11-19 10:12:08 +0800

    chore(cleanup): commonize global env vars

--- push-images.sh
+++ push-images.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
+echo "${DOCKER_PASSWORD}" | docker login -u="${DOCKER_USERNAME}" --password-stdin
 
-IMAGE_NAME=${IMAGE_NAME:-jupyter-pyspark-toree}
-
-TAG_NAME="${SELF_VERSION}_${NOTEBOOK_VERSION}_spark-${SPARK_VERSION}_scala-${SCALA_VERSION}_hadoop-${HADOOP_VERSION}_python-${PYTHON_VERSION}${HIVE_TAG_SUFFIX}_debian"
 docker tag "${IMAGE_NAME}:${TAG_NAME}" "${IMAGE_ORG}/${IMAGE_NAME}:${TAG_NAME}"
 docker push "${IMAGE_ORG}/${IMAGE_NAME}:${TAG_NAME}"
-
-ALT_TAG_NAME="${NOTEBOOK_VERSION}_spark-${SPARK_VERSION}_scala-${SCALA_VERSION}_hadoop-${HADOOP_VERSION}_python-${PYTHON_VERSION}${HIVE_TAG_SUFFIX}_debian"
-docker tag "${IMAGE_NAME}:${TAG_NAME}" "${IMAGE_ORG}/${IMAGE_NAME}:${ALT_TAG_NAME}"
-docker push "${IMAGE_ORG}/${IMAGE_NAME}:${ALT_TAG_NAME}"
